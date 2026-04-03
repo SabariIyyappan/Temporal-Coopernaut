2dff481a9e703411ce0d226bd2e28b21d2a2dbb9
Sabari <sabaripandian3@gmail.com>
2026-04-03 08:02:57 -0700

    launch_carla.sh fixed

--- scripts/launch_carla.sh
+++ scripts/launch_carla.sh
@@ -5,6 +5,6 @@ gpu=$1
 for (( i=1; i<=$2; i++ ))
 do
     port=$((i*$3))
-    CUDA_VISIBLE_DEVICES=$gpu DISPLAY=$DISPLAY $CARLA_ROOT/CarlaUE4.sh -world-port=$port -opengl -nosound -windowed&
+    CUDA_VISIBLE_DEVICES=$gpu DISPLAY=$DISPLAY __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia $CARLA_ROOT/CarlaUE4.sh -world-port=$port -opengl -nosound -windowed&
 done
 wait
